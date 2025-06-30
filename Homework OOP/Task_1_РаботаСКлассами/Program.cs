67e22f509ef29f892f843a3a0eb527ca1c07fcf9
Danik <mr.granddd@gmail.com>
2025-06-30 05:03:41 +0300

    delete unnecessary folders

---
+++ Homework OOP/Task_1_РаботаСКлассами/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_1_РаботаСКлассами;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Player player1 = new Player("Danik", 24, "Archer");
+        var player2 = new Player("Khel", 40, "Warrior");
+        var player3 = new Player();
+
+        player1.Info();
+        player2.Info();
+        player3.Info();
+    }
+
+    private class Player
+    {
+        public readonly string ClassName;
+        public readonly int Lvl;
+        public readonly string Name;
+
+        public Player(string name, int lvl, string className)
+        {
+            Name = name;
+            Lvl = lvl;
+            ClassName = className;
+        }
+
+        public Player()
+        {
+            Name = "Unknown";
+            Lvl = 0;
+            ClassName = "Unknown";
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"\nPlayer - {Name}\nLevel - {Lvl}\nClass - {ClassName}\n");
+        }
+    }
+}
--- Task_2_РаботаСоСвойствами/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task_2_РаботаСоСвойствами;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
