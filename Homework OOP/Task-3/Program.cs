9b8dbf799e120fb210288eb8731d7cc107263f9e
Danik <mr.granddd@gmail.com>
2025-07-05 14:37:28 +0300

    Добавил методы, заработало удаление по ID

--- Homework OOP/Task-3/Program.cs
+++ Homework OOP/Task-3/Program.cs
@@ -4,14 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        var player1 = new Player("Danik", 40);
+        PlayerDataBase players = new PlayerDataBase(); //Создаем пустую БД
         
-        player1.ShowPlayerInfo();
+        var player1 = new Player("Danik", 11);
+        players.AddPlayer(player1);
+        var player2 = new Player("Oleg", 99);
+        players.AddPlayer(player2);
+        var player3 = new Player("Alex", 30);
+        players.AddPlayer(player3);
+        
+        /*player2.ShowPlayerInfo();
+        Console.WriteLine("_____________________________");
+        Console.WriteLine(players.GetPlayerId("Oleg"));*/
+        
+        players.ShowPlayers();
+        players.RemovePlayer(players.GetPlayerId("Oleg"));
+        players.ShowPlayers();
+
+
+
+
+
     }
 
     class PlayerDataBase
     {
-        
+        private List<Player> players = new List<Player>();
+
+        public void ShowPlayers()
+        {
+            foreach (var player in players)
+            {
+                Console.WriteLine("___________________________________");
+                Console.WriteLine($"Name - {player.UserName}\nID - {player.Id}" +
+                                  $"\nLevel - {player.Lvl}\nBan status - {player.IsBanned}");
+                
+            }
+        }
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            players.Remove(player);
+        }
+
+        public Guid GetPlayerId(string name)
+        {
+            foreach (var player in players)
+            {
+                if (player.UserName == name)
+                    return player.Id;
+            }
+            return Guid.Empty; //Метод обязан что-то возвращать
+        }
+
+        public void RemovePlayer(Guid playerId)
+        {
+            int index = 0;
+            foreach (var player in players)
+            {
+                if (player.Id == playerId)
+                    index = players.IndexOf(player);
+            }
+            players.RemoveAt(index);
+        }
         
         
     }
