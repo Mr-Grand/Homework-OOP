c03e83bf3d40aa06096a878da9f6d4a79a4a31b9
Danik <mr.granddd@gmail.com>
2025-07-05 13:51:49 +0300

    Start working with task3

---
+++ Homework OOP/Task-3/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var player1 = new Player("Danik", 40);
+        
+        player1.ShowPlayerInfo();
+    }
+
+    class PlayerDataBase
+    {
+        
+        
+        
+    }
+
+    class Player
+    {
+        public Guid Id { get; }     //свойство только для чтения
+        public string UserName { get; private set; }    //Свойство для изменени только внутри класса
+        public int Lvl { get; private set; }
+        public bool IsBanned { get; private set; }
+
+        public Player(string username, int lvl)
+        {
+            Id = Guid.NewGuid();
+            IsBanned = false;
+            UserName = username;
+            Lvl = lvl;
+        }
+
+        public void ShowPlayerInfo()
+        {
+            Console.WriteLine($"\nName - {UserName}\nID - {Id}\nLevel - {Lvl}\nBan status - {IsBanned}");
+        }
+
+        
+        
+        
+        
+        
+        
+        
+    }
+}
